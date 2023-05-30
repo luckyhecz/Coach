92c5c8d80cc34526ed0fa87225c0b4540a0e5e31
Rustam Shasaidov <luckyhecz@gmail.com>
2023-05-30 15:59:29 +0300

    tasks: fix palindrom function

--- palindrom/index.js
+++ palindrom/index.js
@@ -1,35 +1,9 @@
-//Вариант с консолью
-const process = require('process');
-let input = process.argv[2];
-  let checkSum = 0;
-  for (let i = 0; i < Math.floor(input.length/2); i++) {
-    if (input[i] == input[input.length - 1 -i]) {
-      checkSum++;
+const palindrom = (input) => {
+  for (let i = 0; i < input.length; i++) {
+      if (input[i] !== input[input.length - 1 -i]) {
+      return false;
     }
   }
-  if (Math.floor(input.length/2) == checkSum) {
-    console.log('Да');
-    return true
+    return true;
   }
-  else {
-    console.log('Не');
-    return false
-  }
-
-// Второй вариант с функцией
-// function Palindrom(input) {
-//   let checkSum = 0;
-//   for (let i = 0; i < Math.floor(input.length/2); i++) {
-//     if (input[i] == input[input.length - 1 -i]) {
-//       checkSum++;
-//     }
-//   }
-//   if (Math.floor(input.length/2) == checkSum) {
-//     console.log('Да');
-//     return true
-//   }
-//   else {
-//     console.log('Не');
-//     return false
-//   }
-// }
+  palindrom('шалаш');
